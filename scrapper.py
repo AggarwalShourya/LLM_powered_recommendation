0ee0bfa2a88d40d11eb9506050a18c57e60eb704
AggarwalShourya <165150011+AggarwalShourya@users.noreply.github.com>
2025-04-07 01:55:28 +0530

    Delete scrapper.py

--- scrapper.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-st.title("🧪 Streamlit Test")
-st.markdown("If you see this, your app is working!")
-
-query = st.text_area("Enter something:")
-if st.button("Submit"):
-    st.write(f"You entered: {query}")
